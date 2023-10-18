0e2da8c741be234b7d454175af30e149489a4170
Danilo Martins <dansm86@outlook.com>
2023-10-18 18:01:46 -0300

    feat:added validation to release year for movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ApplicationRecord
   belongs_to :movie_director
   belongs_to :movie_genre
+
+#   validate release_year is an integer and is greater than 1800
+  validates :release_year, numericality: { only_integer: true, greater_than: 1800, less_than: Date.today.year }
+
 end
